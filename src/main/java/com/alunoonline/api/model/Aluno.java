958bc62d25229ccb1afc32a12d9a00623761d9b6
tuliotm <tulioteodoromanso@gmail.com>
2023-07-29 15:38:17 -0300

    Adicionado testes unitários

--- src/main/java/com/alunoonline/api/model/Aluno.java
+++ src/main/java/com/alunoonline/api/model/Aluno.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Entity
 public class Aluno implements Serializable {
 
---
+++ src/test/java/com/alunoonline/api/service/AlunoServiceTest.java
@@ -0,0 +1,97 @@
+package com.alunoonline.api.service;
+
+import com.alunoonline.api.model.Aluno;
+import com.alunoonline.api.repository.AlunoRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class AlunoServiceTest {
+
+    @Mock
+    AlunoRepository alunoRepository;
+
+    @InjectMocks
+    AlunoService alunoService;
+
+    final long ID_ALUNO = 100l;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void listarAlunoPaginado() {
+    }
+
+    @Test
+    void findAll() {
+        Aluno aluno = Aluno.builder()
+                .email("rodrigo@uol.com.br")
+                .nome("Rodrigo Fujioka")
+                .curso("Computação")
+                .id(ID_ALUNO)
+                .build();
+
+        Mockito.when(alunoRepository.findAll())
+                .thenReturn(Arrays.asList(aluno));
+
+        List<Aluno> lista = alunoService.findAll();
+        Assertions
+                .assertEquals(1, lista.size());
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    void findById() {
+        Long ID_CONSULTA = 200l;
+        Aluno aluno = Aluno.builder()
+                .id(ID_CONSULTA)
+                .nome("Rodrigo")
+                .build();
+
+        Mockito.when(alunoRepository.findById(ID_CONSULTA))
+                .thenReturn(Optional.of(aluno));
+        Optional<Aluno> alunoRetorno = alunoService.findById(ID_CONSULTA);
+        Assertions.assertEquals("Rodrigo", alunoRetorno.get().getNome());
+
+    }
+
+    @Test
+    void buscarPorId() {
+        Assertions.assertEquals(1, 2);
+    }
+
+    @Test
+    void delete() {
+        alunoService.delete(ID_ALUNO);
+        Mockito.verify(alunoRepository, Mockito.times(1))
+                .deleteById(ID_ALUNO);
+    }
+
+    @Test
+    void buscarPorNomeIgual() {
+    }
+
+    @Test
+    void buscarPorNomeLike() {
+    }
+}
